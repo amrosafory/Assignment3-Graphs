457045bffe86a481fc5e54c824fe922a5e0b8ace
Amro <amrosafory@aucegypt.edu>
2026-05-15 17:43:57 +0300

    Added name and id comment block to the start of each C++ file

--- Code_library/graph.cpp
+++ Code_library/graph.cpp
@@ -1,3 +1,18 @@
+/********************************************************************************
+* WARNING: Please do not remove or modify this comment block.
+ *
+ * Student Information:
+ * Name: Amro Elsafoury
+ * Student ID: 900242312
+ * Section Number: 01
+ *
+ * Instructions:
+ * - Fill out your name, student ID, and section number above.
+ * - This information is mandatory for the submission of your assignment.
+ * - Do not modify this file beyond the specified tasks and guidelines.
+ *
+ ********************************************************************************/
+
 #include "graph.h"
 #include <iostream>
 
